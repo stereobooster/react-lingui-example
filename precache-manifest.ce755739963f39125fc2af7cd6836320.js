b089bcacff293f32284bd88ef5d3bf1c26bb7662
stereobooster <stereobooster@gmail.com>
2018-11-16 21:14:07 +0100

    Updates

---
+++ precache-manifest.ce755739963f39125fc2af7cd6836320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-lingui-example/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e19e3e4aa2e7373c2c2c",
+    "url": "/react-lingui-example/static/js/runtime~main.e19e3e4a.js"
+  },
+  {
+    "revision": "e0d895b423e2dca9b1be",
+    "url": "/react-lingui-example/static/js/main.e0d895b4.chunk.js"
+  },
+  {
+    "revision": "fa8fffc67584e9063b07",
+    "url": "/react-lingui-example/static/js/i18n-0.fa8fffc6.chunk.js"
+  },
+  {
+    "revision": "7acc9bafb2d3dcb30b0a",
+    "url": "/react-lingui-example/static/js/2.7acc9baf.chunk.js"
+  },
+  {
+    "revision": "e0d895b423e2dca9b1be",
+    "url": "/react-lingui-example/static/css/main.2458bf2d.chunk.css"
+  },
+  {
+    "revision": "7d360750979defcad5519e205f10707a",
+    "url": "/react-lingui-example/index.html"
+  }
+];
