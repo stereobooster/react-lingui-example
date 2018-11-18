aa47c8cba8ded58245e3bff9ff773f4296250712
stereobooster <stereobooster@gmail.com>
2018-11-18 16:58:54 +0100

    Clean up

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { I18nProvider } from "@lingui/react";
 import { Trans } from "@lingui/macro";
 import { i18n, defaultLocale } from "./i18n";
-import logo from "./logo.svg";
-import "./App.css";
 
 i18n.activate(defaultLocale);
 
@@ -11,24 +9,7 @@ class App extends Component {
   render() {
     return (
       <I18nProvider i18n={i18n}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              <Trans>
-                Edit <code>src/App.js</code> and save to reload.
-              </Trans>
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Trans>Learn React</Trans>
-            </a>
-          </header>
-        </div>
+        <Trans>Learn React</Trans>
       </I18nProvider>
     );
   }
