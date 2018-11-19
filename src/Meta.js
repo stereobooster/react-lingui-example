a3736603d94a0de72bace97cb426b72bec665acb
stereobooster <stereobooster@gmail.com>
2018-11-19 23:40:50 +0100

    Simple prerendering

--- src/Meta.js
+++ src/Meta.js
@@ -9,7 +9,7 @@ export default ({ locales }) => (
         htmlAttributes={{ lang: match.params.locale }}
         link={Object.keys(locales).map(locale => {
           const url = match.url.replace(
-            new RegExp(`^/${match.params.locale}`),
+            new RegExp(`/${match.params.locale}($|/)`),
             `/${locale}`
           );
           return { rel: "alternate", href: url, hreflang: locale };
